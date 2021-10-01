860071f7d0cd14dca99b7ddb702b793ebf0af2bb
David Sarkisian <davidsarkisian@b01-aruba-authenticated-10-110-235-93.wlan.cpp.edu>
2021-09-30 23:06:30 -0700

    created comments to outline program

---
+++ numberGame.c
@@ -0,0 +1,12 @@
+/*
+Program Name: numberGame
+Inputs: 1,2 or 3; user guess or new maxValue
+Outputs: Too low, too high, prompt for new guess, or prompt to change max
+
+Variables: user guess, constant for max value, random number from 1-max
+
+Functions: Function to prompt user input
+           Function to do guess/check game
+           Function to set new max value
+
+*/
