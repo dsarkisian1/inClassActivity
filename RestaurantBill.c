f39fdf1b45d5f75dee2eab91cd06657f72abd0c5
David Sarkisian <davidsarkisian@b01-aruba-authenticated-10-110-235-93.wlan.cpp.edu>
2021-09-30 22:57:10 -0700

    Updated code

--- RestaurantBill.c
+++ RestaurantBill.c
@@ -1,21 +1,51 @@
-/*
-Program: RestaurantBill.c
-Inputs: Tax and Tip percentage
-Outputs: The dollar amount of the meal cost, tax/tip, and total bill 
-         of 4 randomly chosen food items
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
 
-
-Variables - 
-Here I will define the set cost of the meals as constant variables
-
-Main - 
-In here I will initialize the tempoary variables to store the user input, as well 
-as the total cost of the bill
-
-I will call the function to prompt the user for their input 
-
-I will call the function to perform the calculations needed
-
-
-
-*/
+int main(){
+  
+   srand(time(NULL));
+   int meal = rand()%4 + 1;
+   double taxPercent;
+   double tipPercent;
+   double mealCost;
+   double tax;
+   double tip;
+   double total;
+   printf("Please enter tax as percentage: ");
+   scanf("%lf",&taxPercent);
+   printf("Please enter tip as percentage: ");
+   scanf("%lf",&tipPercent);
+  
+   if(meal==1){
+       printf("\nMeal Ordered Salad @ $9.95\n");
+       mealCost = 9.95;
+   }
+   else if(meal==2){
+       printf("\nMeal Ordered Soup @ $4.55\n");
+       mealCost = 4.55;
+   }
+   else if(meal==3){
+       printf("\nMeal Ordered Sandwich @ $13.25\n");
+       mealCost = 13.25;
+   }
+   else{
+       printf("\nMeal Ordered Pizza @ $22.35\n");
+       mealCost = 22.35;
+   }
+   tax = mealCost*taxPercent/100;
+   tip = mealCost*tipPercent/100;
+  
+  
+   printf("\n");
+   total = mealCost + tax + tip;
+  
+  
+   printf("Total Tax (%.1lf%%) = $ %12.2lf\n",taxPercent,tax);
+   printf("Tip Amount(%.1lf%%) = $ %12.2lf\n",tipPercent,tip);
+   printf("====================================\n");
+   printf("Total Cost = $ %12.2lf\n",total);
+   printf("====================================\n");
+  
+   return 0;
+}
